d4d50c120c0da8f2a85ab913575bb2721a699816
heogoodman <tjdwo1030@gmail.com>
2023-10-18 18:55:14 +0900

    hsj2

---
+++ src/main/java/com/basic2/dto/CustomerReqDTO.java
@@ -0,0 +1,17 @@
+package com.basic2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CustomerReqDTO {
+
+private String name;
+private String email;
+
+}
